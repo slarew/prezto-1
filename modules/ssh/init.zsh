79c320ceeab7d7fae3280a3ac1007e9943652421
Stephen Larew <stephen@slarew.net>
2018-02-22 11:25:15 -0500

    Use kill (not ps) to find existing ssh-agent
    `ps` and the like are not entirely reliable for finding the running
    ssh-agent.  For example, if procfs on Linux is mounted with hidepid > 0,
    then ps and pgrep appear to fail to find a running ssh-agent under the
    current user.  Sending SIGCONT (continue) should be benign and should
    only succeed if the current user is able to signal the pid (either USER
    owns SSH_AGENT_PID process or USER is root).  It's possible ssh-agent
    with pid SSH_AGENT_PID died and left SSH_AUTH_SOCK behind and a new
    receives SSH_AGENT_PID and would successfully receive SIGCONT, in which
    case we'll just let the user resolve this issue manually since it should
    be rare and rather unlikely.

--- modules/ssh/init.zsh
+++ modules/ssh/init.zsh
@@ -35,10 +35,22 @@ if [[ ! -S "$SSH_AUTH_SOCK" ]]; then
   # Try to grab previously exported environment variables.
   source "$_ssh_agent_env" 2> /dev/null
 
-  # Do not start ssh-agent if the PID from the last start of ssh-agent exists and
-  # corresponds to a running ssh-agent under the current user.
-  if ! ps -U "$LOGNAME" -o pid,comm | grep -E -q -e "^[[:blank:]]*${SSH_AGENT_PID:--1}[[:blank:]].*ssh-agent$"; then
-    eval "$(ssh-agent | sed '/^echo /d' | tee "$_ssh_agent_env")"
+  # Do not start ssh-agent if the socket and PID from the last start of
+  # ssh-agent are still good.
+  if [[ ! -S "$SSH_AUTH_SOCK" || ! -O "$SSH_AUTH_SOCK" ]]; then
+    # NB: `ps` and the like are not entirely reliable for finding the running
+    # ssh-agent.  For example, if procfs on Linux is mounted with hidepid > 0,
+    # then ps and pgrep appear to fail to find a running ssh-agent under the
+    # current user.  Sending SIGCONT (continue) should be benign and should
+    # only succeed if the current user is able to signal  the pid (either USER
+    # owns SSH_AGENT_PID process or USER is root).  It's possible ssh-agent
+    # with pid SSH_AGENT_PID died and left SSH_AUTH_SOCK behind and a new
+    # process receives SSH_AGENT_PID and would successfully receive SIGCONT, in
+    # which case we'll just let the user resolve this issue manually since it
+    # should be rare and rather unlikely.
+    if ! kill -CONT "${SSH_AGENT_PID:-BADPID}" 2> /dev/null ; then
+      eval "$(ssh-agent | sed '/^echo /d' | tee "$_ssh_agent_env")"
+    fi
   fi
 fi
 
